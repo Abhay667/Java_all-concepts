b8a10ab6c236d3bb11e436ea76038984c9c5fa23
Abhay667 <abhayagrawal.cmp@gmail.com>
2023-12-28 23:10:17 +0530

    28-12-23--11:10

---
+++ src/Operator23.java
@@ -0,0 +1,16 @@
+
+public class Operator23 {
+
+	public static void main(String[] args) {
+		double result1 =0/0.0;
+		
+		System.out.println(10<  result1);
+		System.out.println(10<=  result1);
+		System.out.println(10>  result1);
+		System.out.println(10>=  result1);
+		System.out.println(10==  result1);
+		System.out.println(result1==  result1);
+
+	}
+
+}
---
+++ src/Operator24.java
@@ -0,0 +1,11 @@
+
+public class Operator24 {
+
+	public static void main(String[] args) {
+double result1 =0/0.0;
+		
+		System.out.println(10!= result1);
+		System.out.println(result1 != result1);
+	}
+
+}
