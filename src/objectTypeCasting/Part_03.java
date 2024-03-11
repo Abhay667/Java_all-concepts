e887a2b704e7657e1d8191aeef0242bc22995f14
Abhay667 <abhayagrawal.cmp@gmail.com>
2024-03-11 23:32:26 +0530

    11-03-24 11:32

--- src/objectTypeCasting/Part_03.java
+++ src/objectTypeCasting/Part_03.java
@@ -1,12 +1,10 @@
 package objectTypeCasting;
 
- 
-
 public class Part_03 {
 
 	public static void main(String[] args) {
 		Object o = new String("typecasting");
-		StringBuffer sb = (StringBuffer)o;
+		StringBuffer sb = (StringBuffer) o;
 
 	}
 
--- src/objectTypeCasting/Part_04.java
+++ src/objectTypeCasting/Part_04.java
@@ -5,7 +5,8 @@ public class Part_04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String s = new String("durga");
+StringBuffer sb =(StringBuffer)s;
 	}
 
 }
